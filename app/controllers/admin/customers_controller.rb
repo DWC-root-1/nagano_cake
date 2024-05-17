e74ea7983dd94d10cbcb1cb1568b190f950764a3
3rarara <161809435+3rarara@users.noreply.github.com>
2024-05-17 08:41:42 +0000

    admin-sessions

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,3 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
 end
--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,3 @@
 class Admin::GenresController < ApplicationController
+  before_action :authenticate_admin!
 end
