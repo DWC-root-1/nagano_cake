ef22bccffe964b4a24e723d856661495d2d20778
Hiroyuki Tokiya <163287178+tokiya03@users.noreply.github.com>
2024-05-18 01:17:39 +0000

    add product related screen

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,40 @@
 class Admin::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      redirect_to admin_item_path(params[:id])
+    else
+      render :new
+    end
+  end
+
+  def new
+    @item = Item.new
+    @genres = Genre.all
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    @genre = Genre.find(params[:genre_id])
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    @order_detail = Order_detail.find(params[:order_detail_id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
+  # ストロングパラメータ
+  private
+  def item_params
+    params.require(:item).permit(:item_image, :name, :introduction, :genre_id, :price, :is_active)
+  end
 end
