d22afed7f7b7d153fa46e40548fc76ebd843acb5
Hiroyuki Tokiya <163287178+tokiya03@users.noreply.github.com>
2024-05-20 13:40:29 +0000

    fixed admin item and cart-item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,7 +2,7 @@ class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @items = Item.all
+    @items = Item.page(params[:page])
   end
 
   def create
