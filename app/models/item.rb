4133db333937f61f5171b7bf8605a20ed7df0efb
Hiroyuki Tokiya <163287178+tokiya03@users.noreply.github.com>
2024-05-22 05:24:08 +0000

    fixed item

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,17 +3,17 @@ class Item < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   belongs_to :genre
-  has_one_attached :image
+  has_one_attached :item_image
 
   def with_tax_price
     (price * 1.1).floor
   end
 
-  def get_image(width, height)
-    unless image.attached?
+  def get_item_image(width, height)
+    unless item_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_limit: [width, height]).processed
+    item_image.variant(resize_to_fill: [width, height]).processed
   end
 end
