373ed45ec017b6113f595c722642c135f253fb28
3rarara <161809435+3rarara@users.noreply.github.com>
2024-05-23 08:50:54 +0000

    feature/validates

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,15 +7,17 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
+    # もし元々カート内に「同じ商品」がある場合、「数量を追加」更新・保存する
     if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
       cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
       cart_item.amount += params[:cart_item][:amount].to_i
       cart_item.save
       redirect_to cart_items_path
-    else
-      @cart_item.save
+    elsif @cart_item.save
       @cart_items = current_customer.cart_items.all
-      redirect_to cart_items_path
+      render 'index'
+    else
+      render 'index'
     end
   end
 
