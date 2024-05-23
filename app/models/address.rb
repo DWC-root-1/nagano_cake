1517482b0415c27780f5ae1989e555bf1a5d01fb
Hiroyuki Tokiya <163287178+tokiya03@users.noreply.github.com>
2024-05-23 01:05:48 +0000

    fixed item image size

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,7 +2,7 @@ class Address < ApplicationRecord
   belongs_to :customer
 
   def address_display
-    '〒' + post_code + ' ' + address + ' ' + name
+    '〒' + post_code + ' ' + address + '　' + name
   end
 
 end
